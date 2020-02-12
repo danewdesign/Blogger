a0ac4a1f39cfe7d7416e9f799d018e5aac67db1f
Amed Moreno <32321092+danewdesign@users.noreply.github.com>
2020-02-12 15:18:51 -0500

    Blogger Studies in Vue
    First upload/commit

---
+++ routes.js
@@ -0,0 +1,8 @@
+import Home from './Home.vue'
+import BlogEdit from './components/BlogEdit.vue'
+
+export const routes = [
+    {path: '/', component: Home},
+    {path: '/editor', component: BlogEdit}
+];
+
