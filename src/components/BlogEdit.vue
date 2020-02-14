3bac4bcd0512a537121979c3e572bede13a3155d
Amed Moreno <32321092+danewdesign@users.noreply.github.com>
2020-02-14 14:24:34 -0500

    Add files via upload

---
+++ src/components/BlogEdit.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="col-sm-9 col-md-9 col-lg-9 mx-auto mt-4">
+       <h2>New Post</h2>
+       <form class="form-group mt-4">
+           <label for="blogTitle"><h4>Blog Title</h4></label>
+           <input 
+                type="text" 
+                class="form-control" 
+                id="blogTitle"
+                v-model="blogTitle">
+                <p>{{blogTitle}}</p>
+
+           <label class="mt-3" for="blogBody"><h4>Blog Body</h4></label>
+           <textarea 
+                class="form-control no-resize" 
+                name="entry" 
+                id="blogBody" 
+                cols="30" 
+                rows="10"
+                v-model="blogBody"></textarea>
+                <p>{{blogBody}}</p>
+            <button 
+                class="btn btn-lg btn-dark mt-3"
+                @click="postBlog">Post Blog</button>
+       </form>
+    </div>
+</template>
+
+<script>
+import {eventBus} from '../main'
+
+export default {
+    data() {
+        return {
+            blogTitle: '',
+            blogBody: ''
+        }
+    },
+    methods: {
+        postBlog() {
+            let blogObj = JSON.parse(this.blogTitle);
+            eventBus.$emit('newBlogTitle', blogObj);
+            console.log(blogObj);
+        }
+    },
+    computed: {
+  
+    }
+}
+</script>
+
+<style scoped>
+    .no-resize {
+        resize: none;
+    }
+</style>
---
+++ src/components/History.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="mt-2">
+       <h4 class="ml-3">History</h4> 
+    </div>
+</template>
+
+<style scoped>
+
+</style>
