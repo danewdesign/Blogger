31d9729cc153c40e8661600687e9eb6b2f2cf7ef
Amed Moreno <32321092+danewdesign@users.noreply.github.com>
2020-02-14 14:24:03 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+     <blog-header></blog-header>
+     <div class="container-fluid">
+         <router-view></router-view>
+     </div>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+
+export default {
+  computed: {
+ 
+  },
+  components: {
+    "blog-header": Header
+  }
+};
+</script>
+
+<style>
+    .v-hr {
+        border-left: solid 2px lightslategray;
+    
+    }
+</style>
